4e0dd453c58e5feaac51f3d8428f1790a0f3f2cc
pome-ta <pometarou93@gmail.com>
2021-05-09 01:53:20 +0900

    node pack

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+
+// Setup
+const app = express();
+const port = process.env['WEB_APP_PORT'] || 3000;
+
+app.use(express.static(__dirname + '/src'));
+
+// Launch app
+app.listen(port, () => {
+  console.log(`Launching app... http://localhost:${port}` + '\n');
+  //console.log({app});
+});
